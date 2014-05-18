6b83525a08330a1da0a8d4113c5fd25a5360e3cb
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2014-05-18 18:25:22 +0200

    code cleanup and docstrings added

--- msm.py
+++ msm.py
@@ -2,58 +2,93 @@
 # -*- coding: utf-8 -*-
 import sys
 from gi.repository import Gtk, Gio, GObject
+from gi.repository.GLib import GError
 import msmgui.main
-import core.database
-import msmgui.dialogs.preferences
+from core.config import Configuration
+from core.database import Database
+from msmgui.dialogs.preferences import PreferencesDialog
 
 class MagazineSubscriptionManager( Gtk.Application ):
     def __init__( self ):
-        Gtk.Application.__init__( self, application_id="apps.holzhaus.magazinesubscriptionmanager",
-                                 flags=Gio.ApplicationFlags.FLAGS_NONE )
+        """
+        __init__ function.
+        """
+        Gtk.Application.__init__( self,
+                                  application_id="apps.holzhaus.magazinesubscriptionmanager",
+                                  flags=Gio.ApplicationFlags.FLAGS_NONE )
         self.set_property( "register-session" , True )
         self.connect( "activate", self.on_activate )
         self.connect( "startup", self.on_startup )
     def on_activate( self, data=None ):
-        if len( app.get_windows() ):
-            app.get_windows()[0].present()
+        """
+        Callback for the "activate"-signal of the Gtk.Application.
+        Presents the MainWindow if the application is already running.
+        Arguments:
+            data:
+                additional data passed to this function
+        """
+        if len( self.get_windows() ):
+            self.get_windows()[0].present()
     def on_startup( self, data=None ):
-        self.config = core.config.Configuration()
-        self.database = core.database.Database( self.config.get( "Database", "db_uri" ) )
-
-        # a builder to add the UI designed with Glade to the grid:
+        """
+        Callback for the "startup"-signal of the Gtk.Application.
+        Initializes the GUI of the application.
+        Arguments:
+            data:
+                additional data passed to this function
+        """
+        self.config = Configuration()
+        db_uri = self.config.get( "Database", "db_uri" )
+        self.database = Database( db_uri )
+        # Start building the GUI
         builder = Gtk.Builder()
-        # get the file (if it is there)
-        try:
+        try: # get the file (if it is there)
             builder.add_from_file( "data/ui/menu.ui" )
-        except:
+        except GError:
             print( "file not found" )
             sys.exit()
-        # we use the method Gtk.Application.set_menubar(menubar) to add the menubar
-        # to the application (Note: NOT the window!)
+        # We use the method Gtk.Application.set_menubar(menubar) to
+        # add the menubar to the application (Note: NOT the window!)
         self.set_menubar( builder.get_object( "menubar" ) )
         self.set_app_menu( builder.get_object( "appmenu" ) )
         self._mainwindow = msmgui.main.MainWindow( self )
-        self._preferencesdialog = msmgui.dialogs.preferences.PreferencesDialog( self._mainwindow )
+        self._preferencesdialog = PreferencesDialog( self._mainwindow )
         self.add_window( self._mainwindow )
-
-        """AppMenu Actions"""
+        # AppMenu Actions
         settings_action = Gio.SimpleAction.new( "settings", None )
         settings_action.connect( "activate", self.settings_cb )
         self.add_action( settings_action )
-
         quit_action = Gio.SimpleAction.new( "quit", None )
         quit_action.connect( "activate", self.quit_cb )
         self.add_action( quit_action )
         self._mainwindow.show_all()
-
-    """Callbacks for AppMenu Actions"""
+    # Callbacks for AppMenu Actions
     def settings_cb( self, action, parameter ):
+        """
+        Callback for the "settings"-action of the AppMenu.
+        Shows the preferences dialog.
+        Arguments:
+            action:
+                the Gio.SimpleAction that emitted the "activate" signal
+            parameter:
+                the parameter to the activation
+        """
         self._preferencesdialog.show()
     def quit_cb( self, action, parameter ):
+        """
+        Callback for the "quit"-action of the AppMenu.
+        Tries to close the CustomerEditor and quits.
+        Arguments:
+            action:
+                the Gio.SimpleAction that emitted the "activate" signal
+            parameter:
+                the parameter to the activation
+        """
         if self.mainwindow.customereditor.endEdit():
             Gtk.main_quit()
             sys.exit()
+
 if __name__ == "__main__":
-    GObject.threads_init()
-    app = MagazineSubscriptionManager()
-    app.run( None )
+    GObject.threads_init() # Yup, we use threading in this application ;-)
+    msmapp = MagazineSubscriptionManager()
+    msmapp.run( None )
--- msmgui/main.py
+++ msmgui/main.py
@@ -6,15 +6,22 @@ import msmgui.widgets.invoicewindow
 import msmgui.dialogs.about
 class MainWindow( Gtk.ApplicationWindow ):
     def __init__( self, application ):
+        """
+        __init__ function.
+        Arguments:
+            application:
+                the Gtk.Application that this window belongs to
+        """
         Gtk.ApplicationWindow.__init__( self, application=application )
         self.set_property( "window_position", Gtk.WindowPosition.CENTER )
         self.set_property( "default_width", 900 )
         self.set_property( "default_height", 650 )
-        """FIXME: Setting an icon from stock is quite hackish.
-           Read this if you want more details:
-           https://mail.gnome.org/archives/gtk-app-devel-list/2007-September/msg00163.html"""
-        image = Gtk.Image();
-        pixbuf = image.render_icon( "gtk-find-and-replace", Gtk.IconSize.DIALOG, None );
+        # FIXME: Setting an icon from stock is quite hackish.
+        # Read this if you want more details:
+        # https://mail.gnome.org/archives/gtk-app-devel-list/2007-September/msg00163.html
+        image = Gtk.Image()
+        pixbuf = image.render_icon( "gtk-find-and-replace",
+                                    Gtk.IconSize.DIALOG, None )
         self.set_default_icon( pixbuf )
         # Set the window title
         self.set_title( "POSITIONs-Manager" )
@@ -34,27 +41,65 @@ class MainWindow( Gtk.ApplicationWindow ):
         self._invoicewindow.connect( "status-changed", self.statusbar_cb )
 
         self._aboutdialog = msmgui.dialogs.about.AboutDialog( self )
-
-        """MenuBar Actions"""
+        # MenuBar Actions
         about_action = Gio.SimpleAction.new( "about", None )
         about_action.connect( "activate", self.about_cb )
         self.add_action( about_action )
-
         notebook = self.builder.get_object( "notebook" )
         page = self.builder.get_object( "customerwindow" )
-        self.notebook_switch_page_cb( notebook, page, notebook.get_current_page() ) # Call the loading routine manually for first time
+        # Call the loading routine manually for first time
+        self.notebook_switch_page_cb( notebook,
+                                      page,
+                                      notebook.get_current_page() )
     def add_status_message( self, message ):
+        """
+        Adds a status message to the statusbar of the MainWindow.
+        Arguments:
+            message:
+                the status message string to add.
+        """
         statusbar = self.builder.get_object( "statusbar" )
         context_id = statusbar.get_context_id( message )
         statusbar.push( context_id, message )
         return context_id
-    """Callbacks for MenuBar Actions"""
+    # Callbacks for MenuBar Actions
     def about_cb( self, action, parameter ):
+        """
+        Callback for the "about"-action of the MenuBar. Shows the about dialog.
+        Arguments:
+            action:
+                the Gio.SimpleAction that emitted the "activate" signal
+            parameter:
+                the parameter to the activation
+        """
         self._aboutdialog.show()
-    """Callbacks"""
+    # Callbacks
     def statusbar_cb( self, sender, message ):
+        """
+        Callback for child widgets that emit the "status-changed" signal.
+        The emitted messages will be passed to the add_status_message
+        method of the MainWindow.
+        Arguments:
+            sender:
+                the child widget that emitted the "status_changed" signal
+            message:
+                the status message string to add.
+        """
         self.add_status_message( message )
     def notebook_switch_page_cb( self, notebook, page, page_num, user_data=None ):
+        """
+        Callback for the "switch-page" signal of the MainWindow's Gtk.Notebook.
+        Refreshes the child windows if needed.
+        Arguments:
+            notebook:
+                MainWindow's Gtk.Notebook
+            page:
+                the child widget the notebook switched to
+            page_num:
+                the page number the notebook switched to
+            user_data:
+                additional data passed to the callback function
+        """
         if page_num == notebook.page_num( self.builder.get_object( "customerwindow" ) ):
             self._customerwindow.refresh()
         elif page_num == notebook.page_num( self.builder.get_object( "invoicewindow" ) ):
