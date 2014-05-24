b65857b67aac753bffc6fa3b134d88a26000f045
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2014-05-24 21:02:22 +0200

    Some changes that make this script a little bit less horrible :-/
    But it is still dirty as hell!

--- tools/msmxmlimporter.py
+++ tools/msmxmlimporter.py
@@ -1,8 +1,33 @@
 import sys
-sys.path = ['..'] + sys.path
 import os
+if __name__ == "__main__":
+    sys.path = [os.path.abspath( '..' )] + sys.path
 import locale
-from lxml import etree
+try:
+    from lxml import etree
+    print( "running with lxml.etree" )
+except ImportError:
+    try:
+        # Python 2.5
+        import xml.etree.cElementTree as etree
+        print( "running with cElementTree on Python 2.5+" )
+    except ImportError:
+        try:
+            # Python 2.5
+            import xml.etree.ElementTree as etree
+            print( "running with ElementTree on Python 2.5+" )
+        except ImportError:
+            try:
+                # normal cElementTree install
+                import cElementTree as etree
+                print( "running with cElementTree" )
+            except ImportError:
+                try:
+                    # normal ElementTree install
+                    import elementtree.ElementTree as etree
+                    print( "running with ElementTree" )
+                except ImportError:
+                    print( "Failed to import ElementTree from any known place" )
 import dateutil.parser
 import core.config
 import core.database
