d507073dc89146983b611dbbd4f697fa71d95dbc
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2014-05-30 01:02:17 +0200

    Use abspath in Config

--- core/config.py
+++ core/config.py
@@ -10,7 +10,7 @@ import string
 
 CONFIG_DIRNAME = "msm"
 CONFIG_FILENAME = "msm.cfg"
-CONFIG_APPDIR = os.path.join( os.path.dirname( __file__ ), os.pardir )
+CONFIG_APPDIR = os.path.abspath( os.path.join( os.path.dirname( __file__ ), os.pardir ) )
 
 class Configuration( object ):
     def __init__( self ):
